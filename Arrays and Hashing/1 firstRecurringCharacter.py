037f35a9c9dfb2074ad62912d3e0744f8b79f6b3
Sneha Singhi <singhi.sneha98@gmail.com>
2022-07-04 14:08:19 +0530

    Code Updated for First Recurring Character

--- Arrays and Hashing/1 firstRecurringCharacter.py
+++ Arrays and Hashing/1 firstRecurringCharacter.py
@@ -1,12 +1,25 @@
 # https://www.geeksforgeeks.org/find-the-first-repeated-character-in-a-string/
-# Time Complexity - O(n), Space Complexity - O(n) 
-# Note - Time for Lookup in Hash Map - O(1)
 
-def firstRecurringCharacter(input):
-    recurring_dict = {}
-    for c in input:
-        if c in recurring_dict.keys():
+def firstRecurringCharacter(s):
+    # Time Complexity - O(n) and Space Complexity - O(n)
+    hash_map = {}
+    for c in s:
+        if c in hash_map.keys():
             return c
         else:
-            recurring_dict[c] = True
+            hash_map[c] = True 
     return -1
+    
+    # Time Complexity - O(n*n)
+    # min_index = float('inf')
+    # output_char = s[0]
+    # for i in range(0, len(s)-1):
+    #     for j in range(i+1, len(s)):
+    #         if s[i] == s[j]:
+    #             if j < min_index:
+    #                 min_index = j
+    #                 output_char = s[i]
+    # if min_index == float('inf'):
+    #     return -1
+    # else:
+    #     return output_char
