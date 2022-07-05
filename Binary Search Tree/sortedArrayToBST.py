a1cb6f78aa075cb8751640076b5791321aa1391f
Sneha Singhi <singhi.sneha98@gmail.com>
2022-07-05 17:11:56 +0530

    Updated Sorted Array To BST Code

---
+++ Binary Search Tree/sortedArrayToBST.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def createBST(self,l,r,nums):
+    if l<=r:
+        mid = (l+r)//2
+        root = TreeNode(nums[mid])
+        root.left = self.createBST(l, mid-1, nums)
+        root.right = self.createBST(mid+1, r, nums)
+        return root
+    else:
+        return None
