8c9f5e71e01ccbba4ea2124c89bf2d98571112da
Sneha Singhi <singhi.sneha98@gmail.com>
2022-07-06 16:41:20 +0530

    Fibonacci Number Code Udated

---
+++ Dynamic Programming/fibonacciNumber.py
@@ -0,0 +1,12 @@
+def fib(self, n: int): 
+    return self.fibDp(n, {})
+
+def fibDp(self, n, fibDict):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n in fibDict.keys():
+        return fibDict[n]
+    fibDict[n] = self.fibDp(n-1,fibDict) + self.fibDp(n-2, fibDict)
+    return fibDict[n]
